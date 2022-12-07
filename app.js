ade93c265504269b9a7b554d03466b5a5605688b
Matthew English <m6english@gmail.com>
2022-12-07 10:26:56 -0500

    Created Variables/added stubs

---
+++ app.js
@@ -0,0 +1,35 @@
+const keyboard = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+const btnReset = document.getElementsByClassName('btn__reset');
+let missed = 0;
+const phrases = [
+'I am Beyonce always',
+'Thats what she said',
+'Identity theft is not a joke Jim',
+'Who is justice beaver',
+'Dunder Mifflin Paper Company'
+];
+
+const getRandomPhraseAsArray = arr => {
+
+}
+
+const addPhraseToDisplay = arr => {
+
+}
+
+const checkLetter = button => {
+
+}
+
+const checkWin = () => {
+
+}
+
+btnReset.addEventListener('click' () => {
+    
+})
+
+keyboard.addEventListener('click', e => {
+
+})
